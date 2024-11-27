8219ce4728ee5520247e9d4b77f5d67843b3d17f
Claes Engelin <claes.engelin@zocom.se>
2024-11-27 10:18:46 +0100

    Add project files.

---
+++ ConsumeWebApi/Program.cs
@@ -0,0 +1,10 @@
+﻿HttpClient client = new()
+{
+    BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
+};
+
+var response = await client.GetAsync("posts/1");
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
+
+

83cf244e5d4f22f46ff9cdac7a17e48473563b2c
Claes Engelin <claes.engelin@zocom.se>
2024-11-27 11:21:21 +0100

    Lagt till kod för att skapa ny post

---
+++ ConsumeWebApi/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsumeWebApi;
+public class Post
+{
+    [property: JsonPropertyName("userId")]
+    public int UserId { get; set; }
+
+    [property: JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [property: JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [property: JsonPropertyName("body")]
+    public string Body { get; set; }
+
+    public override string ToString()
+    {
+        return $"Post Id:{this.Id} \n UserId:{this.UserId} \n Title: {this.Title} \n Body: {this.Body}";
+    }
+}
--- ConsumeWebApi/Program.cs
+++ ConsumeWebApi/Program.cs
@@ -1,10 +1,77 @@
-﻿HttpClient client = new()
+﻿// Koden finns i Github. Klona från https://github.com/KYH-DSO24/12-ConsumeWebApiDEMO.git
+
+
+
+using ConsumeWebApi;
+
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+
+HttpClient client = new()
 {
     BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
 };
 
-var response = await client.GetAsync("posts/1");
-var content = await response.Content.ReadAsStringAsync();
-Console.WriteLine(content);
+Console.WriteLine("Hämta en post");
+var post = await GetPostAsync(client);
+
+Console.WriteLine(post);
+
+Console.WriteLine("\n\nHämta alla poster:");
+var posts = await GetPostsAsync(client);
+//foreach (var p  in posts)
+//{
+//    Console.WriteLine(p);
+//}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(posts[i]);
+    Console.WriteLine();
+}
+
+Console.WriteLine("\n\nSkapa en post");
+post = new Post()
+{
+    UserId = 1,
+    Title = "Test",
+    Body = "Test kropp med lite mer text\noch en radbrytning, bara för att vi kan"
+};
+var response = await SendPostAsync(client, post);
+Console.WriteLine(response);
+
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
+Console.ReadKey();
+
+
+async Task<Post> GetPostAsync(HttpClient client)
+{
+    await using Stream stream =
+        await client.GetStreamAsync("posts/1");
+    var result = await JsonSerializer.DeserializeAsync<Post>(stream);
+
+    return result ?? new();
+}
+
+static async Task<List<Post>> GetPostsAsync(HttpClient client)
+{
+    await using Stream stream =
+        await client.GetStreamAsync("posts");
+    var result = await JsonSerializer.DeserializeAsync<List<Post>>(stream);
+
+    return result ?? new();
+}
+
+static async Task<Post> SendPostAsync(HttpClient client, Post post)
+{
+    string jsonString = JsonSerializer.Serialize(post);
+    HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+    var response = await client.PostAsync("/posts", content);
+
+    response.EnsureSuccessStatusCode();
+
+    var result = await response.Content.ReadFromJsonAsync<Post>();
+    return result;
+}
 
 
